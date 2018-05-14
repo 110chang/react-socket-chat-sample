6fdbb2c5633da439087bd0d875514dcce4ab015d
110chang <u@110chang.com>
2018-05-14 16:43:41 +0900

    index.js tweak

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 //index.js
 
-var Color = require('color');
+var Color = require('color')
 
-var express = require('express');
-var app = express();
-var http = require('http').Server(app);
-var socketio = require('socket.io');
+var express = require('express')
+var app = express()
+var http = require('http').Server(app)
+var socketio = require('socket.io')
 
-var io = socketio(http);
+var io = socketio(http)
 
-var uid = 0;
-var commentId = 0;
-var users = [];
-var color = Color('#F00');
+var uid = 0
+var commentId = 0
+var users = []
+var color = Color('#F00')
 
-app.use(express.static('dist'));
+app.use(express.static('dist'))
 
-io.on('connection', function(socket){
-  console.log('user connected', socket.id);
+io.on('connection', function (socket) {
+  console.log('user connected', socket.id)
 
   io.to(socket.id).emit('chat login', {
     userId: ++uid,
     userColor: color.rotate(30).darken(0.3).hex()
-  });
+  })
 
   io.to(socket.id).emit('chat message', {
     type: 'system',
@@ -30,7 +30,7 @@ io.on('connection', function(socket){
     text: '#sys ' + uid + '番さんこんにちは！',
     _id: commentId++,
     createdOn: Date.now()
-  });
+  })
 
   socket.broadcast.emit('chat message', {
     type: 'system',
@@ -38,32 +38,32 @@ io.on('connection', function(socket){
     text: '#' + uid + '番さんが入室しました',
     _id: commentId++,
     createdOn: Date.now()
-  });
-  users[socket.id] = uid;
+  })
+  users[socket.id] = uid
 
-  socket.on('chat message', function(msg, id, date, color){
-    console.log('receive msg ', msg);
+  socket.on('chat message', function (msg, id, date, color) {
+    console.log('receive msg ', msg)
     io.emit('chat message', {
       type: 'user',
       color: color,
       text: '#' + id + ' : ' + msg,
       _id: commentId++,
       createdOn: date
-    });
-  });
+    })
+  })
 
-  socket.on('disconnect', function(){
-    console.log('user disconnected', socket.id);
+  socket.on('disconnect', function () {
+    console.log('user disconnected', socket.id)
     io.emit('chat message', {
       type: 'system',
       color: '#666',
       text: '#sys ' + users[socket.id] + '番さんが退室しました',
       _id: commentId++,
       createdOn: Date.now()
-    });
-  });
-});
+    })
+  })
+})
 
-http.listen(3000, function() {
-  console.log('listen *:3000');
-});
+http.listen(3000, function () {
+  console.log('listen *:3000')
+})
